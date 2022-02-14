550dd6a335e69fb809cf5f431d629c1cbdcf0d27
Joblyn <oaikhenahjob@gmail.com>
2022-02-14 09:39:10 +0100

    readme update

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,10 +2,10 @@ import axios from "axios";
 import * as ENDPOINTS from "../constants/endpoints";
 
 const baseURL = "https://api.paystack.co";
+const SECRET_KEY = "sk_test_3ce090bffd566f4365b38410accfd494a0d96a71";
 const headers = {
   "Content-Type": "application/json",
-  // "Authorization": `Bearer ${process.env.API_SECRET_KEY}`,
-  Authorization: "Bearer sk_test_3ce090bffd566f4365b38410accfd494a0d96a71",
+  Authorization: "Bearer " + SECRET_KEY,
 };
 
 export const getAllBanks = axios.get(baseURL + ENDPOINTS.GETALLBANKS, {
